b34b7f7401afbfda22337b1cdb5d3d934ec80fce
Levente Danel <danellevente@yahoo.com>
2020-10-21 19:06:24 +0300

    add more content and improve responsiveness

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const menuItems = document.querySelectorAll(".navigation__link");
 const menuCheckbox = document.querySelector("#navi-toggle");
 
-menuItems.forEach(item => item.addEventListener("click", toggleMenu));
+menuItems.forEach((item) => item.addEventListener("click", toggleMenu));
 
 function toggleMenu() {
   menuCheckbox.checked = !menuCheckbox.checked;
